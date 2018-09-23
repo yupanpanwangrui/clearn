ea37c497529a91ea1daa2f8bc0a1160c5eea5aa6
yupanpan <1378026306@qq.com>
2018-09-23 18:12:01 +0800

    modify multi

--- least_square.cpp
+++ least_square.cpp
@@ -4,29 +4,45 @@
 int main()
 {
 	using namespace std;
-	double Init_y[3] = {1,2,3};
-    	double Init_x[3][2] = {{0,0},{0,1},{1,0}};
-	int Max_step = 100;
-	double Init_learn = 0.1;
-	double Beta[2] = {0.1,0.1};	
+	double Init_y[4] = {1,2,3,4};
+    double Init_x[4][3] = {{0,0,0},{0,1,0},{1,0,0},{0,0,0}};
+	int Max_step = 1000;
+	double Init_learn = 0.01;
+	double Beta[3] = {0.1,0.1,0.1};	
 	for(int i=0;i<Max_step;i++)
 	{
-		double sum_beta_x = 0;
-		double Gradient[2] = {0,0};
 		double sum = 0;
 		double y_beta_x = 0;
+		double Gradient[3] = {0,0,0};
 		for(int j=0;j<sizeof(Init_x)/sizeof(Init_x[0]);j++)
 		{
-            		sum_beta_x = Beta[0]*Init_x[j][0] + Beta[1]*Init_x[j][1];
+			double sum_beta_x = 0;
+			
+			for(int s=0; s<sizeof(Init_x[0])/sizeof(Init_x[0][0]); s++)
+			{
+				sum_beta_x = sum_beta_x + Beta[s]*Init_x[j][s];
+			}
+            		
 			double temp = 2*(Init_y[j]-sum_beta_x);
-			Gradient[0] = Gradient[0] + temp*(-Init_x[j][0]);
-			Gradient[1] = Gradient[1] + temp*(-Init_x[j][1]);
+			for(int s=0; s<sizeof(Init_x[0])/sizeof(Init_x[0][0]); s++)
+			{
+				Gradient[s] = Gradient[s] + temp*(-Init_x[j][s]);
+			}
+			
+		}
+		for(int s=0; s<sizeof(Init_x[0])/sizeof(Init_x[0][0]); s++)
+		{
+			Beta[s]	= Beta[s] - Gradient[s] * Init_learn;
 		} 
-		Beta[0]	= Beta[0] - Gradient[0] * Init_learn;
-		Beta[1] = Beta[1] - Gradient[1] * Init_learn;
+		
 		for(int m=0;m<sizeof(Init_x)/sizeof(Init_x[0]);m++)
 		{
-			y_beta_x = Init_y[m] - (Beta[0]*Init_x[m][0]+Beta[1]*Init_x[m][1]);
+			double temp = 0;
+			for(int s=0; s<sizeof(Init_x[0])/sizeof(Init_x[0][0]); s++)
+			{
+				temp = temp + Beta[s]*Init_x[m][s]; 
+			}
+			y_beta_x = Init_y[m] - temp;
 			sum = sum + y_beta_x*y_beta_x;
 
 		}
