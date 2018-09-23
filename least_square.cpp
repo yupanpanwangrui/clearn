77b00ec914b5d33c0e955d75776173ab2a6f0735
yupanpan <1378026306@qq.com>
2018-09-23 11:14:46 +0800

    finish, but the result has error

--- least_square.cpp
+++ least_square.cpp
@@ -1,27 +1,37 @@
 #include<math.h>
+#include<iostream>
 
-using namespace std;
-
-void main()
+int main()
 {
-	double* Init_y = {1,2,3};
-    double* Init_x = {0,0;0,1;1,0};
-	int Max_step = 100;
+	using namespace std;
+	double Init_y[3] = {1,2,3};
+    	double Init_x[3][2] = {{0,0},{0,1},{1,0}};
+	int Max_step = 10;
 	double Init_learn = 0.001;
-	double* Beta = {0.1,0.1};
-	double* Gradient = {0,0};
-
+	double Beta[2] = {0.1,0.1};
+	double Gradient[2] = {0,0};
+	double sum = 0;
+	double sum_beta_x = 0;
+	double y_beta_x = 0;
 	for(int i=0;i<Max_step;i++)
 	{
-		for(int j=0;j<len(Init_x);j++)
+		for(int j=0;j<sizeof(Init_x)/sizeof(Init_x[0]);j++)
 		{
-            sum_beta_x = Beta*(Init_x+j).0 + (Beta+1)*(Init_x+j).1;
-			temp = 2*((Init_y+j)-sum_beta_x);
-			Gradient = Gradient + temp*(Init_x+j).0;
-			Gradient+1 = (Gradient+1) + temp*(Init_x+j).1;
+            		sum_beta_x = Beta[0]*Init_x[j][0] + Beta[1]*Init_x[j][1];
+			double temp = 2*(Init_y[j]-sum_beta_x);
+			Gradient[0] = Gradient[0] + temp*(-Init_x[j][0]);
+			Gradient[1] = Gradient[1] + temp*(-Init_x[j][1]);
 		} 
-		Beta = Beta - Gradient * Init_learn;
-		Beta+1 = (Beta+1) - (Gradient+1) * Init_learn;
+		Beta[0]	= Beta[0] - Gradient[0] * Init_learn;
+		Beta[1] = Beta[1] - Gradient[1] * Init_learn;
+		for(int m=0;m<sizeof(Init_x)/sizeof(Init_x[0]);m++)
+		{
+			y_beta_x = Init_y[m] - (Beta[0]*Init_x[m][0]+Beta[1]*Init_x[m][1]);
+			sum = sum + y_beta_x*y_beta_x;
+
+		}
+		cout << "The loss of " << i << " step is " << sum <<";" << endl;
 	}
+	return 0;
 
 }
